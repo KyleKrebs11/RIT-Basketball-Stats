a45a05ddfc2ab1f1d72c724f6d8f641a8876f698
kylekrebs <kylekrebs11@yahoo.com>
2023-11-20 01:44:15 -0500

    Added some planning for stats to analyze

--- Data-Analysis/python.py
+++ Data-Analysis/python.py
@@ -1,5 +1,17 @@
-# @author: Kyle Krebs
-# @author: Nick Creeley
-#
-# file: python.py
-# Use as an example for pull requests.
+import mysql_connector
+
+try:
+    db_host = "localhost"
+    db_username = "root"
+    db_password = "14063"
+
+    connection = mysql_connector(
+        host = db_host,
+        user = db_username,
+        password = db_password
+    )
+
+    print("Connection successful")
+    connection.close
+except:
+    print("connection Failed")
