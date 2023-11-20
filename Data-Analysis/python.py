944adee8e5e566c79d2ab4fd01cc66160847a21d
Nick <nc8004@rit.edu>
2023-11-20 18:56:30 -0500

    fixed, mysql connecter import: should be mysql.connecter

--- Data-Analysis/python.py
+++ Data-Analysis/python.py
@@ -1,11 +1,11 @@
-import mysql_connector
+import mysql.connector
 
 try:
     db_host = "localhost"
     db_username = "root"
     db_password = "14063"
 
-    connection = mysql_connector(
+    connection = mysql.connector.connect(
         host = db_host,
         user = db_username,
         password = db_password
