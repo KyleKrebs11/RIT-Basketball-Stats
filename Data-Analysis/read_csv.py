dea2cebd7dcfecf5d46f53c0283f898dd3a9af4d
Nick <nc8004@rit.edu>
2024-01-27 12:58:03 -0500

    Created basic csv reading

--- Data-Analysis/read_csv.py
+++ Data-Analysis/read_csv.py
@@ -1,7 +1,41 @@
+"""
+File: read_csv.py
+
+Description:
+A program to read a given csv file from command line, 
+parse and grab needed information to later be added to database
+
+Authors:
+Nick Creeley
+Kyle Krebs
+
+"""
+
 import pandas as pd
+import sys
 
 
-def main():
-    df = pd.read_sql_table("elmira_playbyplay")
-    print(df.head)
-    return 0
+"""
+process_csv: Processes the csv file
+
+"""
+def process_csv(csv_file_path):
+    
+    #turn csv into pandas dataframe
+    df = pd.read_csv(csv_file_path)
+
+    print(df)
+
+
+if __name__ == "__main__":
+    
+    #check that file is provided
+    if len(sys.argv) < 2:
+        print("Usage: python read_csv.py <csv file path_1> <csv file path_2>...")
+        sys.exit(1)
+    
+    #process csv files given 
+    for csv_path in sys.argv[1:]:
+        #Process each csv path
+        process_csv(csv_path)
+     
